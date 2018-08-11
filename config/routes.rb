1176490e270f30a383abb9275cf6e7b3ab78a5fb
Sly Verano <slyverano@gmail.com>
2018-08-11 21:45:19 +1000

    Add contact page and services sub-menu

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   match 'services', to: 'static_pages#services', via: :get
   match 'faq', to: 'static_pages#faq', via: :get
   match 'about', to: 'static_pages#about', via: :get
+  match 'contact', to: 'static_pages#contact', via: :get
 end
