48ec1b2781e9b4c125ceb718153adfe8d0b515fd
Sly Verano <slyverano@gmail.com>
2018-09-24 00:24:53 +1000

    Added the services single pages

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,4 +4,28 @@ class ServicesController < ApplicationController
 
   def lawn_care
   end
+
+  def grass_and_hedge_cutting
+  end
+
+  def garden_waste_removal
+  end
+
+  def garden_shed_making
+  end
+
+  def roof_gutter_cleaning
+  end
+
+  def fencing_and_fence_painting
+  end
+
+  def weeding
+  end
+
+  def planting
+  end
+
+  def landscaping
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,15 @@ Rails.application.routes.draw do
   scope path: 'services' do
   	root 'services#index', as: 'services'
   	get 'lawn-care', to: 'services#lawn_care'
+    get 'grass-and-hedge-cutting', to: 'services#grass_and_hedge_cutting'
+    get 'garden-waste-removal', to: 'services#garden_waste_removal'
+    get 'garden-shed-making', to: 'services#garden_shed_making'
+    get 'roof-gutter-cleaning', to: 'services#roof_gutter_cleaning'
+    get 'garden-maintenance', to: 'services#garden_maintenance'
+    get 'fencing-and-fence-painting', to: 'services#fencing_and_fence_painting'
+    get 'weeding', to: 'services#weeding'
+    get 'planting', to: 'services#planting'
+    get 'landscaping', to: 'services#landscaping'
   end
 
   match 'faq', to: 'static_pages#faq', via: :get
